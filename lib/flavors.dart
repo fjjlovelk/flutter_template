77c6725856c498117537af7c9b990975391ea1bd
fjjlovelk <2730809743@qq.com>
2024-10-22 16:18:40 +0800

    修改 app name

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -9,17 +9,16 @@ class F {
 
   static String get name => appFlavor?.name ?? '';
 
-  /// app标题
   static String get title {
     switch (appFlavor) {
       case Flavor.env_development:
-        return 'flutter_template（开发）';
+        return 'flutterTemplate（开发）';
       case Flavor.env_test:
-        return 'flutter_template（测试）';
+        return 'flutterTemplate（测试）';
       case Flavor.env_production:
-        return 'flutter_template';
+        return 'flutterTemplate';
       default:
-        return 'flutter_template';
+        return 'flutterTemplate';
     }
   }
 
